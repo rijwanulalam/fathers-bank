9f275cfbd9b82186514766c916aeeab5cdd0f671
rijwanulalam <rijwanul007@gmail.com>
2023-11-12 21:42:17 +0600

    deposit and withdraw added

---
+++ js/bank.js
@@ -0,0 +1,9 @@
+const depositwMoney = document.getElementById("deposit-money").innerText;
+
+
+const withdrawMoney = document.getElementById("withdraw-money").innerText;
+
+
+const balance = document.getElementById("balance-money").innerText;
+
+console.log(depositwMoney, withdrawMoney, balance)
